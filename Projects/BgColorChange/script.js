f953119bb37d9b587053f6cb6585767a9694e49d
Pruthvi <pruthavirajsuryawanshi07@gmail.com>
2024-11-27 00:39:08 +0530

    Background Change Using DOM

---
+++ Projects/BgColorChange/script.js
@@ -0,0 +1,11 @@
+
+const body = document.querySelector('body')
+
+const buttons = document.querySelectorAll('.button')
+
+buttons.forEach((button)=>{
+  button.addEventListener('click', (e)=>{
+    body.style.backgroundColor = e.target.id;
+  })
+})
+
